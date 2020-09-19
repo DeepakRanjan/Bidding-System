e09f82a160173696ed9f7cf41dcdc187ef90a542
Deepak Ranjan <deepakranjan@Deepaks-MacBook-Pro.local>
2020-09-19 21:51:56 +0530

    initial setup

---
+++ src/test/java/com/bidding/system/Bidding/System/BiddingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidding.system.Bidding.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
